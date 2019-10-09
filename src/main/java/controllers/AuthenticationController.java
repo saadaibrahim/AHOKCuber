15ce2ee5429351f4245c478f5eac6e2fcee980eb
Adnan Hamzeh <adnan.37h@gmail.com>
2019-10-09 22:36:37 +0300

    Create properties file and implement JWT authentication
    Implemented by Osman

--- src/main/java/controllers/AuthenticationController.java
+++ src/main/java/controllers/AuthenticationController.java
@@ -1,14 +1,15 @@
 package controllers;
 
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import utils.Config;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
 
 @Path("/api/auth")
 public class AuthenticationController {
@@ -16,9 +17,15 @@ public class AuthenticationController {
     @Path("/token")
     @Produces("application/json")
     public Response all() {
-        //RSA
-        Algorithm algorithmHS = Algorithm.HMAC256("secret");
-
-        return Response.ok("clients list goes here", MediaType.APPLICATION_JSON).build();
+        String token = "";
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(Config.getProperty("AUTH_PASSPHRASE"));
+            token = JWT.create()
+                    .withIssuer("cuber")
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            //Invalid Signing configuration / Couldn't convert Claims.
+        }
+        return Response.ok(token, MediaType.APPLICATION_JSON).build();
     }
 }
--- src/main/java/utils/Config.java
+++ src/main/java/utils/Config.java
@@ -1,6 +1,6 @@
 package utils;
 
-import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -9,15 +9,28 @@ public class Config {
 
     private Properties prop;
 
-//    public Config (String $configName) {
-//        InputStream input = new FileInputStream("");
-//
-//        this.prop = new Properties();
-//
-//        try {
-//            prop.load(input);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    public Config (String configName) {
+        InputStream input = null;
+        this.prop = new Properties();
+        try {
+            input = Config.class.getClassLoader().getResourceAsStream(configName + ".properties");
+            prop.load(input);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String configName, String propertyName) {
+        Config config = new Config(configName);
+
+        return config.prop.getProperty(propertyName);
+    }
+
+    public static String getProperty(String propertyName) {
+        Config config = new Config("config");
+
+        return config.prop.getProperty(propertyName);
+    }
 }
